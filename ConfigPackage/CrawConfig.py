b48088a05c21910b2c2dab1f8c5d2d071defc6fd
NintendoLink <18705192873@163.com>
2017-08-23 16:22:27 +0800

    1、add content pagecroll 2、add config info

---
+++ ConfigPackage/CrawConfig.py
@@ -0,0 +1,11 @@
+HOME='http://www.dianping.com'
+ORIGIN_PAGE=''
+OUT_PATH_DIC=''
+HEADERS= {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.86 Safari/537.36',
+    'Cookie': 'navCtgScroll=0; showNav=#nav-tab|0|0; navCtgScroll=0; showNav=#nav-tab|0|0; _hc.v="\"28c48835-807a-422e-ba61-ad744498f777.1450593856\""; __utma=1.196682546.1450594182.1450594182.1452605452.2; __utmz=1.1452605452.2.2.utmcsr=gufensoso.com|utmccn=(referral)|utmcmd=referral|utmcct=/search/; PHOENIX_ID=0a018986-152531ce5a3-13cb4e7; s_ViewType=10; JSESSIONID=6FAF186D727AFC4CD60107EBA6D2D2D4; aburl=1; cy=1; cye=shanghai'
+    }
+
+PROXIES={'http': "117.175.193.42:8123",
+           "https": "http://10.10.1.10:1080"
+         }
---
+++ LearnPython.py
@@ -0,0 +1,4 @@
+# from ConfigPackage import CrawConfig as config
+#
+# print config.HEADERS
+
